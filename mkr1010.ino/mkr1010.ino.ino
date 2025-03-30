aec35ac85093864d8873ece4a50827fb86c29b4f
Joshua Farmer <rhubarbjosh@outlook.com>
2025-03-30 20:58:01 +0100

    screen

--- mkr1010.ino/mkr1010.ino.ino
+++ mkr1010.ino/mkr1010.ino.ino
@@ -1,7 +1,9 @@
 /* simple emulator for programs */
+#include <Elegoo_GFX.h>    // Core graphics library
+#include <Elegoo_TFTLCD.h> // Hardware-specific library
 
 #define MEM_ADDR(addr) ((addr) & (MEM_SIZE - 1))
-#define MEM_SIZE 1024
+#define MEM_SIZE 512
 #define CONSUME_IMMEDIATE_8() pc += 1;
 #define CONSUME_IMMEDIATE_16() pc += 2;
 
@@ -16,7 +18,7 @@ const byte INT_PINS[] = {2, 3, 4, 5, 6, 7, 8, 9};
 byte interrupts_mask;
 byte interrupt_vector = 0;
 
-byte *MEM=NULL;
+byte *MEM = NULL;
 byte memory[MEM_SIZE];
 
 /* no need for NOP instruction as its just e.g. LD B,B*/
@@ -91,6 +93,97 @@ inline byte construct(byte opcode, byte dst)
         return (opcode & 31) | (dst << 5);
 }
 
+int x = 0;
+int y = 0;
+
+#define LCD_CS A3          // Chip Select goes to Analog 3
+#define LCD_CD A2          // Command/Data goes to Analog 2
+#define LCD_WR A1          // LCD Write goes to Analog 1
+#define LCD_RD A0          // LCD Read goes to Analog 0
+#define LCD_RESET A4       // Can alternately just connect to Arduino's reset pin
+
+// When using the BREAKOUT BOARD only, use these 8 data lines to the LCD:
+// For the Arduino Uno, Duemilanove, Diecimila, etc.:
+//   D0 connects to digital pin 8  (Notice these are
+//   D1 connects to digital pin 9   NOT in order!)
+//   D2 connects to digital pin 2
+//   D3 connects to digital pin 3
+//   D4 connects to digital pin 4
+//   D5 connects to digital pin 5
+//   D6 connects to digital pin 6
+//   D7 connects to digital pin 7
+// For the Arduino Mega, use digital pins 22 through 29
+// (on the 2-row header at the end of the board).
+
+// Assign human-readable names to some common 16-bit color values:
+#define BLACK 0x0000
+#define BLUE 0x001F
+#define RED 0xF800
+#define GREEN 0x07E0
+#define CYAN 0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW 0xFFE0
+#define WHITE 0xFFFF
+
+Elegoo_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+
+void setup_lcd(void)
+{
+        Serial.print("TFT size is ");
+        Serial.print(tft.width());
+        Serial.print("x");
+        Serial.println(tft.height());
+        tft.reset();
+        uint16_t identifier = tft.readID();
+        if (identifier == 0x9325)
+        {
+                Serial.println(F("Found ILI9325 LCD driver"));
+        }
+        else if (identifier == 0x9328)
+        {
+                Serial.println(F("Found ILI9328 LCD driver"));
+        }
+        else if (identifier == 0x4535)
+        {
+                Serial.println(F("Found LGDP4535 LCD driver"));
+        }
+        else if (identifier == 0x7575)
+        {
+                Serial.println(F("Found HX8347G LCD driver"));
+        }
+        else if (identifier == 0x9341)
+        {
+                Serial.println(F("Found ILI9341 LCD driver"));
+        }
+        else if (identifier == 0x8357)
+        {
+                Serial.println(F("Found HX8357D LCD driver"));
+        }
+        else if (identifier == 0x0101)
+        {
+                identifier = 0x9341;
+                Serial.println(F("Found 0x9341 LCD driver"));
+        }
+        else if (identifier == 0x1111)
+        {
+                identifier = 0x9328;
+                Serial.println(F("Found 0x9328 LCD driver"));
+        }
+        else
+        {
+                Serial.print(F("Unknown LCD driver chip: "));
+                Serial.println(identifier, HEX);
+                Serial.println(F("If using the Elegoo 2.8\" TFT Arduino shield, the line:"));
+                Serial.println(F("  #define USE_Elegoo_SHIELD_PINOUT"));
+                Serial.println(F("should appear in the library header (Elegoo_TFT.h)."));
+                Serial.println(F("If using the breakout board, it should NOT be #defined!"));
+                Serial.println(F("Also if using the breakout, double-check that all wiring"));
+                Serial.println(F("matches the tutorial."));
+                identifier = 0x9328;
+        }
+        tft.begin(identifier);
+}
+
 void execute(byte *memory)
 {
         byte inst = memory[MEM_ADDR(pc++)];
@@ -102,8 +195,8 @@ void execute(byte *memory)
         ushort wz = (regs[6] << 8) | regs[7];
         byte *deref_cd = &memory[cd];
         byte imm8 = memory[MEM_ADDR(pc)];
-        ushort imm16 = (imm8 << 8) | memory[MEM_ADDR(pc+1)];
-        ushort extimm16 = (memory[MEM_ADDR(pc+1)] << 8) | memory[MEM_ADDR(pc+2) & (MEM_SIZE - 1)];
+        ushort imm16 = (imm8 << 8) | memory[MEM_ADDR(pc + 1)];
+        ushort extimm16 = (memory[MEM_ADDR(pc + 1)] << 8) | memory[MEM_ADDR(pc + 2) & (MEM_SIZE - 1)];
         byte *arg_reg = &regs[short_arg];
         switch (opcode)
         {
@@ -277,28 +370,28 @@ void execute(byte *memory)
                 case INST_LXAB:
                 {
                         short_arg = short_arg & 3;
-                        regs[short_arg * 2]     = regs[0];
+                        regs[short_arg * 2] = regs[0];
                         regs[short_arg * 2 + 1] = regs[1];
                         break;
                 }
                 case INST_LXCD:
                 {
                         short_arg = short_arg & 3;
-                        regs[short_arg * 2]     = regs[2];
+                        regs[short_arg * 2] = regs[2];
                         regs[short_arg * 2 + 1] = regs[3];
                         break;
                 }
                 case INST_LXEF:
                 {
                         short_arg = short_arg & 3;
-                        regs[short_arg * 2]     = regs[4];
+                        regs[short_arg * 2] = regs[4];
                         regs[short_arg * 2 + 1] = regs[5];
                         break;
                 }
                 case INST_LXWZ:
                 {
                         short_arg = short_arg & 3;
-                        regs[short_arg * 2]     = regs[6];
+                        regs[short_arg * 2] = regs[6];
                         regs[short_arg * 2 + 1] = regs[7];
                         break;
                 }
@@ -306,13 +399,34 @@ void execute(byte *memory)
                 {
                         CONSUME_IMMEDIATE_16();
                         short_arg = short_arg & 3;
-                        regs[short_arg * 2]   = extimm16 >> 8;
+                        regs[short_arg * 2] = extimm16 >> 8;
                         regs[short_arg * 2 + 1] = extimm16;
                         break;
                 }
                 case INST_OUT_R:
                 {
-                        Serial.print((char)*arg_reg);
+                        if (regs[7] == 0)
+                        {
+                                Serial.print((char)*arg_reg);
+                        }
+                        else if (regs[7] == 1)
+                        {
+                                x = regs[7];
+                        }
+                        else if (regs[7] == 2)
+                        {
+                                y = regs;
+                        }
+                        else if (regs[7] == 3)
+                        {
+                                tft.setCursor(x, y);
+                                tft.print((char)*arg_reg);
+                                x+=16;
+                                if (x >= 240)
+                                {
+                                        x = 0; y+=16;
+                                }
+                        }
                         break;
                 }
                 case INST_INP_R:
@@ -396,54 +510,120 @@ void dump_registers()
         ushort cd = (regs[2] << 8) | regs[3];
         ushort ef = (regs[4] << 8) | regs[5];
         ushort wz = (regs[6] << 8) | regs[7];
-        Serial.print("AB="); Serial.println(ab, HEX);
-        Serial.print("CD="); Serial.println(cd, HEX);
-        Serial.print("EF="); Serial.println(ef, HEX);
-        Serial.print("WZ="); Serial.println(wz, HEX);
-        Serial.print("PC="); Serial.println(pc, HEX);
-        Serial.print("SP="); Serial.println(sp, HEX);
+        Serial.print("AB=");
+        Serial.println(ab, HEX);
+        Serial.print("CD=");
+        Serial.println(cd, HEX);
+        Serial.print("EF=");
+        Serial.println(ef, HEX);
+        Serial.print("WZ=");
+        Serial.println(wz, HEX);
+        Serial.print("PC=");
+        Serial.println(pc, HEX);
+        Serial.print("SP=");
+        Serial.println(sp, HEX);
 }
 
 void save()
-{       if (!MEM) return;
+{
+        if (!MEM)
+                return;
         MEM[sp--] = pc;
         MEM[sp--] = ZE;
         MEM[sp--] = CY;
         MEM[sp--] = interrupts_mask;
 }
 
-void interruptHandler0() { if (interrupt_vector & 0b00000001) { save(); pc = 0*8; } }
-void interruptHandler1() { if (interrupt_vector & 0b00000010) { save(); pc = 1*8; } }
-void interruptHandler2() { if (interrupt_vector & 0b00000100) { save(); pc = 2*8; } }
-void interruptHandler3() { if (interrupt_vector & 0b00001000) { save(); pc = 3*8; } }
-void interruptHandler4() { if (interrupt_vector & 0b00010000) { save(); pc = 4*8; } }
-void interruptHandler5() { if (interrupt_vector & 0b00100000) { save(); pc = 5*8; } }
-void interruptHandler6() { if (interrupt_vector & 0b01000000) { save(); pc = 6*8; } }
-void interruptHandler7() { if (interrupt_vector & 0b10000000) { save(); pc = 7*8; } }
+void interruptHandler0()
+{
+        if (interrupt_vector & 0b00000001)
+        {
+                save();
+                pc = 0 * 8;
+        }
+}
+void interruptHandler1()
+{
+        if (interrupt_vector & 0b00000010)
+        {
+                save();
+                pc = 1 * 8;
+        }
+}
+void interruptHandler2()
+{
+        if (interrupt_vector & 0b00000100)
+        {
+                save();
+                pc = 2 * 8;
+        }
+}
+void interruptHandler3()
+{
+        if (interrupt_vector & 0b00001000)
+        {
+                save();
+                pc = 3 * 8;
+        }
+}
+void interruptHandler4()
+{
+        if (interrupt_vector & 0b00010000)
+        {
+                save();
+                pc = 4 * 8;
+        }
+}
+void interruptHandler5()
+{
+        if (interrupt_vector & 0b00100000)
+        {
+                save();
+                pc = 5 * 8;
+        }
+}
+void interruptHandler6()
+{
+        if (interrupt_vector & 0b01000000)
+        {
+                save();
+                pc = 6 * 8;
+        }
+}
+void interruptHandler7()
+{
+        if (interrupt_vector & 0b10000000)
+        {
+                save();
+                pc = 7 * 8;
+        }
+}
+
+///*tft.fillScreen(BLACK);*/
+//tft.setCursor(0, 0);
+//tft.setTextColor(RED);
+//tft.setTextSize(1);
+//tft.println("Hello World!");
 
 void setup()
 {
         Serial.begin(9600);
+        setup_lcd();
+        tft.fillScreen(BLACK);
+        tft.setTextSize(2);
+        tft.setTextColor(WHITE);
         /*LDI A,1*/
         /*LDI B,3*/
         /*ADD B*/
 
-        attachInterrupt(digitalPinToInterrupt(INT_PINS[0]), interruptHandler0, FALLING);
-        attachInterrupt(digitalPinToInterrupt(INT_PINS[1]), interruptHandler1, FALLING);
-        attachInterrupt(digitalPinToInterrupt(INT_PINS[2]), interruptHandler2, FALLING);
-        attachInterrupt(digitalPinToInterrupt(INT_PINS[3]), interruptHandler3, FALLING);
-        attachInterrupt(digitalPinToInterrupt(INT_PINS[4]), interruptHandler4, FALLING);
-        attachInterrupt(digitalPinToInterrupt(INT_PINS[5]), interruptHandler5, FALLING);
-        attachInterrupt(digitalPinToInterrupt(INT_PINS[6]), interruptHandler6, FALLING);
-        attachInterrupt(digitalPinToInterrupt(INT_PINS[7]), interruptHandler7, FALLING);
-
-        MEM=memory;
+        MEM = memory;
         memory[0] = construct(INST_LDI, 0);
-        memory[1] = 1;
-        memory[2] = construct(INST_LDI, 1);
+        memory[1] = 65;
+        memory[2] = construct(INST_LDI, 7);
         memory[3] = 3;
-        memory[4] = construct(INST_ADD, 1);
-        memory[5] = construct(INST_HLT, 0);
+        memory[4] = construct(PREFIX_1C, 0);
+        memory[5] = construct(INST_OUT_R, 0);
+        memory[6] = construct(INST_HLT, 0);
 }
 
 void loop()
@@ -452,4 +632,6 @@ void loop()
         {
                 execute(memory);
         }
+
+        delay(100);
 }
